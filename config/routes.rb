92fc9a2cbb5aeccef632bec464df76a70ef8cd38
Pedro Miranda <pbragamiranda@gmail.com>
2020-11-17 18:12:37 -0300

    pushing lost_pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #verb "/path", to: 'controller#action'
+
+  #show all pets
+  get '/pets', to: 'pets#index'
+
+  get '/pets/new', to: 'pets#new'
+
+  get '/pets/:id', to: 'pets#show', as: :pet
+
+  post '/pets', to: 'pets#create'
+
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch 'pets/:id', to: 'pets#update'
+
+  delete 'pets/:id', to: 'pets#destroy', as: :delete_pet
 end
+
+
+# routes -> controller -> view
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+16.times do
+
+  Pet.create(
+    name: Faker::Creature::Dog.name,
+    address: Faker::Address.street_address,
+    species: ['cat', 'dog', 'bird', 'frog'].sample,
+    found_on: Date.today - rand(30)
+    )
+
+end
